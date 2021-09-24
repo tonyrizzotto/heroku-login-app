07492fc32653b830ac0863ef6d06389825ed0fa9
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-09-24 11:14:15 -0400

    Created Profile Card upon login

--- frontend/src/components/ProfileView.js
+++ frontend/src/components/ProfileView.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import './ProfileView.css';
 
 const ProfileView = (props) => {
-  return <div>Hello, {props.user.firstName}</div>;
+  console.log(props);
+  return (
+    <div className="card">
+      <div className="left-side">
+        <div className="profile-image">
+          <img src={props.user.imageUrl} alt={props.user.firstName} />
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="title">
+          <h1>
+            {props.user.firstName} {props.user.lastName}
+          </h1>
+        </div>
+        <div className="user-desc">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="user-email">
+          <b>Contact:</b> {props.user.email}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileView;
