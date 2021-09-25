3db1cdbbace914fac45e15d5dab6ff0bb1b8aea5
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-09-25 11:19:25 -0400

    Switched to a MemoryRouter to hide URLs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <MemoryRouter>
     <App />
-  </BrowserRouter>,
+  </MemoryRouter>,
   document.getElementById('root')
 );
